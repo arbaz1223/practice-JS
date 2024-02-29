0d6f98136b56ec52038b5da56982ad9bde62577d
arbaz1223 <karachi76543214@gmail.com>
2024-02-28 21:12:02 -0800

    practice js

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-// Question1
+// var result = +prompt('enter your total mark');
+// if(result>500) {
+//     console.log ('result is not correct send me the correct mark')
+// }else if(result <200){
+//     console.log( 'stay at home')
+// }
+// else if(result >= 200 && result<=300){
+//     console.log( 'grade is C')
+// }else if(result >= 300 && result<=400){
+//     console.log( 'grade is B')
+// } else{
+//     console.log(result)
+// }
 
-alert("enter your password ")
-
-// Question 2
-
-var password='enter your password'
-console.log( password)
+var Account = +prompt('enter your total amount');
+if(Account>2000) {
+    console.log ('result is not correct send me the correct mark')
+}else if(Account <500){
+    console.log( 'you can not take that amount')
+}
+else if(Account >= 500 && Account<=1000){
+    console.log( 'you can take ')
+}else if(Account >= 1000 && Account<=1500){
+    console.log( 'your account well be closed')
+} else{
+    console.log(Account)
+}
